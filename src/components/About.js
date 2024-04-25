3ca7c536159d4decb78f575fd2d8139d7b53a547
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-04-25 08:45:08 +0300

    props.links added

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Links from "./Links";
-import user from "../data/user";
+import user from "../data/user"
 
 function About(props) {
   return (
@@ -8,7 +8,7 @@ function About(props) {
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links link={props.github} />
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,13 +1,13 @@
 import React from "react";
-import user from "../data/user";
+
 
 
 function Links(props){
     return(
         <>
         <h3>Links</h3>
-        <a href={props.link}>{user.links.github}</a>
-        <a href={props.link}>{user.links.linkedin}</a>
+        <a href={props.github}>{props.github}</a>
+        <a href={props.linkedin}>{props.linkedin}</a>
         </>
     )
 }
